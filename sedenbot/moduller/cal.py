93ea96139553dffe0d9cd0fc47b162abe76ffa44
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2020-06-07 22:19:43 +0300

    improve code base of cal module, for better stability

--- sedenbot/moduller/cal.py
+++ sedenbot/moduller/cal.py
@@ -8,34 +8,62 @@ from time import sleep
 from telethon.tl.functions.channels import InviteToChannelRequest
 from telethon.tl.functions.messages import AddChatUserRequest, DeleteChatUserRequest
 
+stopPullingUsers = False
+
 @sedenify(outgoing=True, pattern=".sa")
 async def _(event):
+    global stopPullingUsers
     await event.edit('Merhabalar herkese')
-    limit = extract_args(event)
-    print(f'limit {limit}') 
+    args = extract_args(event).split(' ')
+    notifyUser = bool(args[1]) if len(args) > 1 else True
+    limit = int(args[0]) if isinstance(args[0], int) else 1000
+
     sleepAfterAwhile = []
-    async for user in event.client.iter_participants(event.chat_id, limit=int(limit)):
-        if isinstance(user.status, UserStatusOnline):
-            if not user.bot and not user.contact and not user.is_self:
-                print(user.first_name)
-                try:
-                    result = await event.client(AddContactRequest(
-                        id=int(user.id),
-                        first_name=str(user.first_name),
-                        last_name=str(user.last_name) if user.last_name else '',
-                        phone='' if not user.phone else str(user.phone),
-                        add_phone_privacy_exception=False
-                    ))
-                    sleepAfterAwhile.append(user)
-                    sendToUser = await bot.get_entity(339388824)
-                    await bot.send_message(sendToUser, f'@{user.username or user.first_name} `kullanicisini dizladim oglim`')
-                    if len(sleepAfterAwhile) > 4:
-                        print('Uyuyuorum suan ', len(sleepAfterAwhile))
-                        sleep(61)
-                        sleepAfterAwhile.clear()
-                except Exception as err:
-                    # e = sys.exc_info()[0]
-                    print(err)
+    async for user in event.client.iter_participants(event.chat_id, limit=int(limit), filter=ChannelParticipantsRecent):
+        
+        if stopPullingUsers:
+            break
+        
+        
+        if not user.bot and not user.contact and not user.is_self:
+            print(user.first_name)
+            try:
+                result = await event.client(AddContactRequest(
+                    id=int(user.id),
+                    first_name=str(user.first_name),
+                    last_name=str(user.last_name) if user.last_name else '',
+                    phone='' if not user.phone else str(user.phone),
+                    add_phone_privacy_exception=False
+                ))
+                sleepAfterAwhile.append(user)
+                
+                
+                if len(sleepAfterAwhile) > 4:
+                    if notifyUser:
+                        await _sendMessageToMainAccount(sleepAfterAwhile)
+                    sleep(61)
+                    sleepAfterAwhile.clear()
+
+
+            except Exception as err:
+                LOGS.error(err)
+                _sendMessageToMainAccount(user, str(err))
+
+async def _sendMessageToMainAccount(users, message = ''):
+    msg = message
+    if not message:
+        msg = ' , '.join(f'@{user.username}' or user.first_name for user in users) + ' Dizladimm...'
+
+    LOGS.info(msg)
+    sendToUser = await bot.get_entity(339388824)
+    await bot.send_message(sendToUser, msg)
+
+
+@sedenify(outgoing=True, pattern='.kes')
+async def _stopPullingUsers(event):
+    global stopPullingUsers
+    stopPullingUsers = True
+    await event.edit('Ben kactim arkadaslar, kendinize iyi baghin')
 
 
 @sedenify(outgoing=True, pattern='.as')
