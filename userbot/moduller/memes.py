63b9078d5a50a8c2e340f90f8984477f08f8123a
userbot Updater <>
2020-06-09 18:29:21 +0300

    add ddg search meme

--- userbot/moduller/memes.py
+++ userbot/moduller/memes.py
@@ -3,7 +3,7 @@ import asyncio
 from userbot.events import extract_args, register
 from asyncio import sleep
 from collections import deque
-
+from userbot import CMD_HELP
 
 
 @register(outgoing=True, pattern="^.hack")
@@ -15,21 +15,21 @@ async def port_hack(event):
     animation_interval = 3
     animation_ttl = range(0, 11)
     #input_str = event.pattern_match.group(1)
-    #if input_str == "hack":
+    # if input_str == "hack":
     await event.edit("Hacking..")
     animation_chars = [
-            "`Connecting To Hacked Private Server...`",
-            "`Target Selected.`",
-            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
-            "`Hacking... 100%\n█████████HACKED███████████ `",
-            f"`Targeted Account Hacked by @hasanisabbah...\n\n {message} `"
-        ]
+        "`Connecting To Hacked Private Server...`",
+        "`Target Selected.`",
+        "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+        "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+        "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+        "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+        "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+        "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+        "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+        "`Hacking... 100%\n█████████HACKED███████████ `",
+        f"`Targeted Account Hacked by @hasanisabbah...\n\n {message} `"
+    ]
 
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
@@ -39,28 +39,28 @@ async def port_hack(event):
 @register(outgoing=True, pattern="^.mizah$")
 async def mizahshow(e):
     await e.edit(
-    "⚠️⚠️⚠️MmMmMmMizahh Şoww😨😨😨😨😱😱😱😱😱 \n"
-    "😱😱⚠️⚠️ 😂😂😂😂😂😂😂😂😂😂😂😂😂😂😱😵 \n"
-    "😂😂👍👍👍👍👍👍👍👍👍👍👍👍👍 MiZah \n"
-    "ŞeLaLesNdEn b1r yUdm aLdım✔️✔️✔️✔️ \n"
-    "AHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHA \n"
-    "HAHAHAHAHAHAHHAHAHAHAHAHAHA😂😂😂😂😂😂😂😂 \n"
-    "😂 KOMİK LAN KOMİİİK \n"
-    "heLaL LaN ✔️✔️✔️✔️✔️✔️✔️✔️👏👏👏👏👏👏👏👏 \n"
-    "👏 EfSaNe mMmMiZah şooooovv 👏👏👏👏👏😂😂😂😂 \n"
-    "😂😂😂😂😂😂⚠️ \n"
-    "💯💯💯💯💯💯💯💯💯 \n"
-    "KNK AYNI BİİİZ 😂😂😂👏👏 \n"
-    "💯💯⚠️⚠️♿️AÇ YOLU POST SAHİBİ VE ONU ♿️SAVUNANLAR \n"
-    "GELIYOR ♿️♿️ DÜÜTT♿️ \n"
-    "DÜÜÜÜT♿️DÜÜT♿️💯💯⚠️ \n"
-    "♿️KOMİİİK ♿️ \n"
-    "CJWJCJWJXJJWDJJQUXJAJXJAJXJWJFJWJXJAJXJWJXJWJFIWIXJQJJQJASJAXJ \n"
-    "AJXJAJXJJAJXJWJFWJJFWIIFIWICIWIFIWICJAXJWJFJEICIIEICIEIFIWICJSXJJS \n"
-    "CJEIVIAJXBWJCJIQICIWJSX💯💯💯💯💯💯😂😂😂😂😂😂😂 \n"
-    "😂⚠️😂😂😂😂😂😂⚠️⚠️⚠️😂😂😂😂♿️♿️♿️😅😅 \n"
-    "😅😂👏💯⚠️👏♿️🚨"
-    )    
+        "⚠️⚠️⚠️MmMmMmMizahh Şoww😨😨😨😨😱😱😱😱😱 \n"
+        "😱😱⚠️⚠️ 😂😂😂😂😂😂😂😂😂😂😂😂😂😂😱😵 \n"
+        "😂😂👍👍👍👍👍👍👍👍👍👍👍👍👍 MiZah \n"
+        "ŞeLaLesNdEn b1r yUdm aLdım✔️✔️✔️✔️ \n"
+        "AHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHA \n"
+        "HAHAHAHAHAHAHHAHAHAHAHAHAHA😂😂😂😂😂😂😂😂 \n"
+        "😂 KOMİK LAN KOMİİİK \n"
+        "heLaL LaN ✔️✔️✔️✔️✔️✔️✔️✔️👏👏👏👏👏👏👏👏 \n"
+        "👏 EfSaNe mMmMiZah şooooovv 👏👏👏👏👏😂😂😂😂 \n"
+        "😂😂😂😂😂😂⚠️ \n"
+        "💯💯💯💯💯💯💯💯💯 \n"
+        "KNK AYNI BİİİZ 😂😂😂👏👏 \n"
+        "💯💯⚠️⚠️♿️AÇ YOLU POST SAHİBİ VE ONU ♿️SAVUNANLAR \n"
+        "GELIYOR ♿️♿️ DÜÜTT♿️ \n"
+        "DÜÜÜÜT♿️DÜÜT♿️💯💯⚠️ \n"
+        "♿️KOMİİİK ♿️ \n"
+        "CJWJCJWJXJJWDJJQUXJAJXJAJXJWJFJWJXJAJXJWJXJWJFIWIXJQJJQJASJAXJ \n"
+        "AJXJAJXJJAJXJWJFWJJFWIIFIWICIWIFIWICJAXJWJFJEICIIEICIEIFIWICJSXJJS \n"
+        "CJEIVIAJXBWJCJIQICIWJSX💯💯💯💯💯💯😂😂😂😂😂😂😂 \n"
+        "😂⚠️😂😂😂😂😂😂⚠️⚠️⚠️😂😂😂😂♿️♿️♿️😅😅 \n"
+        "😅😂👏💯⚠️👏♿️🚨"
+    )
 
 
 @register(pattern=r".type(?: |$)(.*)", outgoing=True)
@@ -96,3 +96,34 @@ async def _(event):
         await event.edit("".join(deq))
         deq.rotate(1)
     await event.edit("❤️🧡💛" + input_str + "💚💙💜")
+
+
+@register(outgoing=True, pattern=".ddg")
+async def ddg(event):
+    if event.fwd_from:
+        return
+    input_str = extract_args(event)
+    sample_url = "https://duckduckgo.com/?q={}".format(
+        input_str.replace(" ", "+"))
+    if sample_url:
+        link = sample_url.rstrip()
+        await event.edit("**Bir dakika senin için 🦆 DuckDuckGo üzerinden arama yapıyorum**:\n🔎 [{}]({})".format(input_str, link))
+    else:
+
+        await event.edit("Bir şeyler ters gitti...")
+
+
+CMD_HELP.update({
+    "hack":
+    ".hack \
+    \nKullanım: Hacking animasyonudur.\n",
+    "mizah":
+    ".mizah \
+    \nKullanım: Mizah selalesinden bir yudum alin.\n",
+    "type":
+    ".type \
+    \nKullanım: Yazilarinizi animasyonu bir sekilde yazin.\n",
+    "ddg":
+    ".ddg \
+    \nKullanım: Usengecler icin ddg aramasi gerceklestirin.\n",
+})
