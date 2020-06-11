b3ba8a63a40f4b47f58d7a00b1c629ac84c957b8
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2020-06-12 02:35:06 +0300

    add tts command as it is heavly usable

--- userbot/moduller/memes.py
+++ userbot/moduller/memes.py
@@ -18,7 +18,7 @@ import asyncio
 from asyncio import sleep
 from collections import deque
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, BOTLOG_CHATID, BOTLOG
 from userbot.events import extract_args, register
 
 
@@ -130,6 +130,55 @@ async def ddg(event):
         await event.edit("Bir şeyler ters gitti...")
 
 
+@register(outgoing=True, pattern=r"^.tts(?: |$)([\s\S]*)")
+async def text_to_speech(query):
+    from gtts import gTTS
+    from os import remove
+
+    textx = await query.get_reply_message()
+    message = query.pattern_match.group(1)
+    TTS_LANG = "tr"
+
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await query.edit(
+            "`Yazıdan sese çevirmek için bir metin gir.`")
+        return
+
+    try:
+        gTTS(message, lang=TTS_LANG)
+    except AssertionError:
+        await query.edit(
+            'Metin boş.\n'
+            'Ön işleme, tokenizasyon ve temizlikten sonra konuşacak hiçbir şey kalmadı.'
+        )
+        return
+    except ValueError:
+        await query.edit('Bu dil henüz desteklenmiyor.')
+        return
+    except RuntimeError:
+        await query.edit('Dilin sözlüğünü görüntülemede bir hata gerçekleşti.')
+        return
+    tts = gTTS(message, lang=TTS_LANG)
+    tts.save("h.mp3")
+    with open("h.mp3", "rb") as audio:
+        linelist = list(audio)
+        linecount = len(linelist)
+    if linecount == 1:
+        tts = gTTS(message, lang=TTS_LANG)
+        tts.save("h.mp3")
+    with open("h.mp3", "r"):
+        await query.client.send_file(query.chat_id, "h.mp3", voice_note=True)
+        remove("h.mp3")
+        if BOTLOG:
+            await query.client.send_message(
+                BOTLOG_CHATID, "Metin başarıyla sese dönüştürüldü!")
+        await query.delete()
+
+
 CMD_HELP.update({
     "hack":
         ".hack \
@@ -143,4 +192,7 @@ CMD_HELP.update({
     "ddg":
         ".ddg \
         \nKullanım: Usengecler icin ddg aramasi gerceklestirin.\n",
+    "tts":
+        ".tts \
+        \nKullanım: komutu ile Google'ın metinden yazıya dönüştürme servisi kullanılabilir..\n",
 })
