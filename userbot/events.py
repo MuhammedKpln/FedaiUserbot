926ec3306d65813968c43b27d1f3df011e4f63d6
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2020-07-15 18:05:52 +0300

    change ling

--- userbot/events.py
+++ userbot/events.py
@@ -94,7 +94,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT HATA RAPORU**\n"
-                    link = "[Asena Destek Grubu](https://t.me/AsenaSupport)"
+                    link = "[Fedai UserBot Support](https://t.me/FedaiUserBotSupport)"
                     text += "İsterseniz, bunu rapor edebilirsiniz"
                     text += f"- sadece bu mesajı buraya iletin {link}.\n"
                     text += "Hata ve Tarih dışında hiçbir şey kaydedilmez\n"
